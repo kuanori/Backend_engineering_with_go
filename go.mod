b695fa7802ceb5125e0326a2d8cbe6602550d0db
kuanori <kuanyshmykyev@gmail.com>
2025-08-24 14:12:20 +0600

    5-3 persisting data with sql

--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.24.6
 
 require github.com/go-chi/chi v1.5.5
 
+require github.com/lib/pq v1.10.9 // indirect
+
 replace github.com/cosmtrek/air => github.com/air-verse/air v1.62.0
--- internal/repository/posts.go
+++ internal/repository/posts.go
@@ -3,12 +3,47 @@ package repository
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
-type PostsRepository struct {
+// это как моделька в Laravel
+type Post struct {
+	ID        int64    `json:"id"`
+	UserID    int64    `json:"user_id"`
+	Title     string   `json:"title"`
+	Content   string   `json:"content"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
+
+type PostRepository struct {
 	db *sql.DB
 }
 
-func (s *PostsRepository) Create(ctx context.Context) error {
+func (s *PostRepository) Create(ctx context.Context, post *Post) error {
+
+	query := `
+		INSERT INTO posts (user_id, title, content,tags)
+		VALUES ($1, $2, $3, $4) RETURN id, created_at,
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.UserID,
+		post.Title,
+		post.Content,
+		pq.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
