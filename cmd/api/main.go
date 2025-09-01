fa0badd005386e5bb2a49e5c2ce8cea671f04eb4
kuanori <kuanyshmykyev@gmail.com>
2025-09-01 23:00:46 +0600

    feature-6-1-marshaling-json-response

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,6 +16,8 @@ func main() {
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
+		env:     env.GetString("ENV", "development"),
+		version: env.GetString("VERSION", "0.0.1"),
 	} // это как создание обьекта класса в php
 
 	db, err := db.New(
