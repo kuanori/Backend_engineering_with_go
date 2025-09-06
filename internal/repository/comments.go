f17c5f6ab631fbe726025d92fafdb965e999b188
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-06 21:26:40 +0600

    6-7-adding-comments-to-posts

---
+++ internal/repository/comments.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Comment struct {
+	ID        int64  `json:"id"`
+	PostID    int64  `json:"post_id"`
+	UserID    int64  `json:"user_id"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	User      User   `json:"user`
+}
+
+type CommentRepository struct {
+	db *sql.DB
+}
+
+func (s *CommentRepository) GetByPostID(ctx context.Context, postID int64) ([]Comment, error) {
+
+	query := `
+		SELECT c.id, c.post_id, c.user_id, c.content, c.created_at, u.user_name, u.id FROM comments c
+		JOIN users u ON u.id = c.user_id
+		WHERE c.post_id = $1
+		ORDER BY c.created_at DESC;
+	`
+
+	rows, err := s.db.QueryContext(ctx, query, postID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	comments := []Comment{}
+	for rows.Next() {
+		var c Comment
+		c.User = User{}
+		err := rows.Scan(&c.ID, &c.PostID, &c.UserID, &c.Content, &c.CreatedAt, &c.User.Username, &c.User.ID)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+
+	return comments, nil
+}
