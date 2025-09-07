9561b971e5588901b52bfce74ba0cf6ed1fcdfa6
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-07 16:48:46 +0600

    fixed migration| followers

--- cmd/migrate/migrations/000006_create_followers.up.sql
+++ cmd/migrate/migrations/000006_create_followers.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE followers (
+CREATE TABLE IF NOT EXISTS followers (
     user_id bigint NOT NULL,
     follower_id bigint NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
