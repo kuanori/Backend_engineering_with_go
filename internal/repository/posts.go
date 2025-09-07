6c9607e2713687bf97776c18915fd920c9efb7bb
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-07 17:43:53 +0600

    7-4 user feed algorithm

--- internal/repository/posts.go
+++ internal/repository/posts.go
@@ -19,6 +19,12 @@ type Post struct {
 	CreatedAt string    `json:"created_at"`
 	UpdatedAt string    `json:"updated_at"`
 	Comments  []Comment `json:"comments"`
+	User      User      `json"user"`
+}
+
+type PostWithMetadata struct {
+	Post
+	CommentsCount int `json:"commens_count"`
 }
 
 type PostRepository struct {
@@ -26,6 +32,56 @@ type PostRepository struct {
 }
 
 var ErrConflict = errors.New("edit conflict")
+
+func (s *PostRepository) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMetadata, error) {
+
+	query := `
+		SELECT 
+			p.id, p.user_id, p.title, p.content, p.created_at, p.tags, 
+			u.username,
+			COUNT (c.id) AS comments_count
+		FROM posts p
+		LEFT JOIN comments c ON c.post_id = p.id
+		LEFT JOIN users u ON u.id = p.user_id
+		JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
+		WHERE f.user_id = $1
+		GROUP BY p.id, u.username
+		ORDER BY p.created_at DESC;
+		`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var feed []PostWithMetadata
+	for rows.Next() {
+		var p PostWithMetadata
+		err := rows.Scan(
+			&p.ID,
+			&p.UserID,
+			&p.Title,
+			&p.Content,
+			&p.CreatedAt,
+			pq.Array(&p.Tags),
+			&p.User.Username,
+			&p.CommentsCount,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		feed = append(feed, p)
+	}
+
+	return feed, nil
+
+}
 
 func (s *PostRepository) Create(ctx context.Context, post *Post) error {
 
