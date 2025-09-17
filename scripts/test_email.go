4a312c9727c6ab0ce9003add94afc62993955898
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-17 22:03:38 +0600

    12-1 sending email invitation

---
+++ scripts/test_email.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func main() {
+	from := mail.NewEmail("Example User", "kuanyshmykyev@gmail.com")
+	subject := "Sending with SendGrid is Fun"
+	to := mail.NewEmail("Example User", "kuanyshmykyev@gmail.com")
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	// client.Request, _ = sendgrid.SetDataResidency(client.Request, "eu")
+	// uncomment the above line if you are sending mail using a regional EU subuser
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
+}
